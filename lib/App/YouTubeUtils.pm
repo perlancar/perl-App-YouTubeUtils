5188f2cb306792dc8809927735fd733b06c4ee0e
perlancar (@pc-home) <perlancar@gmail.com>
2016-05-08 21:59:42 +0700

    Initial commit

---
+++ lib/App/YouTubeUtils.pm
@@ -0,0 +1,16 @@
+package App::YouTubeUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Command-line utilities related to YouTube
+
+=head1 DESCRIPTION
+
+This distribution provides the following command-line utilities related to
+YouTube:
+
+#INSERT_EXECS_LIST
+
+=cut
